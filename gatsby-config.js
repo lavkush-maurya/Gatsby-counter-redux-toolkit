11f8c0bfa7c596df9ec12a34de2dbace2adfca62
Lavkush Maurya <lavkushmaurya147@gmail.com>
2023-06-19 11:19:09 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `counter-redux`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
